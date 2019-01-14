60715f90db5e27720584060c7c0f27e024b2b0a5
Kirsten Allen <kmaaallen@gmail.com>
2019-01-14 18:00:39 +0000

    rendered github user repository data to page

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,4 +1,4 @@
-function userInformationHTML(user){
+function userInformationHTML(user) {
     return `
     <h2> ${user.name}
     <span class=""small-name">
@@ -18,9 +18,30 @@ function userInformationHTML(user){
     `;
 }
 
-function fetchGitHubInformation(event){
+function repoInformationHTML(repos) {
+    if (repos.length == 0) {
+        return `<div class="clearfix repo-list">No repos!</div>`;
+    }
+
+    var listItemsHTML = repos.map(function(repo) {
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`;
+    });
+
+    return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`;
+};
+
+function fetchGitHubInformation(event) {
     var username = $("#gh-username").val();
-    if(!username){
+    if (!username) {
         $("#gh-user-data").html(`<h2> Please enter a GitHub username</h2>`);
         return;
     }
@@ -28,19 +49,24 @@ function fetchGitHubInformation(event){
         `<div id ="loader">
         <img src = "assets/css/loader.gif" alt = "loading..." />
         </div>`);
-        
-        $.when(
-            $.getJSON(`https://api.github.com/users/${username}`)
-            ).then(
-                function(response){
-                    var userData = response;
-                    $("#gh-user-data").html(userInformationHTML(userData));
-                }, function(errorResponse){
-                    if (errorResponse.status === 404){
-                        $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
-                    }else{
-                        console.log(errorResponse);
-                        $("#gh-user-data").html(`<h2>Error : ${errorResponse.responseJSON.message}</h2>`);
-                    }
-                });
+
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
+    ).then(
+        function(firstResponse, secondResponse) {
+            var userData = firstResponse[0];
+            var repoData = secondResponse[0];
+            $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
+        },
+        function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
+            }
+            else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(`<h2>Error : ${errorResponse.responseJSON.message}</h2>`);
+            }
+        });
 }
